52b364cb514291dc4582e514ee2355bd02cc992d
Omole kessiena David <edokadavid@gmail.com>
2025-03-14 07:51:17 +0100

    Add changes and new files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 
 <x-app-layout pageName="{{$pageName}}">
-
+@include('sections.homepage.hero')
 </x-app-layout>
---
+++ resources/views/sections/homepage/hero.blade.php
@@ -0,0 +1,31 @@
+<div class="hero bg-image parallaxie">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <!-- Hero Content Start -->
+                <div class="hero-content">
+                    <!-- Section Title Start -->
+                    <div class="section-title">
+                        <h3 class="wow fadeInUp">welcome to Solidrock eye specialist clinic</h3>
+                        <h1 class="text-anime-style-2" data-cursor="-opaque">Dental Care for the Whole Family</h1>
+                        <p class="wow fadeInUp" data-wow-delay="0.25s">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal</p>
+                    </div>
+                    <!-- Section Title End -->
+
+                    <!-- Hero Content Body Start -->
+                    <div class="hero-content-body wow fadeInUp" data-wow-delay="0.5s">
+                        <a href="#" class="btn-default">make an appointment</a>
+                    </div>
+                    <!-- Hero Content Body End -->
+
+                    <!-- Google Rating Start -->
+                    <div class="google-rating wow fadeInUp" data-wow-delay="0.75s">
+                        <p class="wow">We are the best at waht we do</p>
+                    </div>
+                    <!-- Google Rating End -->
+                </div>
+                <!-- Hero Content End -->
+            </div>
+        </div>
+    </div>
+</div>
