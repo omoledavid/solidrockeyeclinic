bb56cb6be41617bea988754ef67a43e2c095c5d5
Omole kessiena David <edokadavid@gmail.com>
2025-04-20 23:39:15 +0100

    Update changes

--- resources/views/components/solidrock/faq-card.blade.php
+++ resources/views/components/solidrock/faq-card.blade.php
@@ -1,12 +1,12 @@
 @props(['faq'])
 <div class="accordion-item wow fadeInUp">
-    <h2 class="accordion-header" id="heading1">
+    <h2 class="accordion-header" id="heading.{{$faq->id }}">
         <button class="accordion-button" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
             {{ $faq->question }}
         </button>
     </h2>
-    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1"
+    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading.{{$faq->id }}"
          data-bs-parent="#accordion">
         <div class="accordion-body">
             <p>{{$faq->answer}}</p>
